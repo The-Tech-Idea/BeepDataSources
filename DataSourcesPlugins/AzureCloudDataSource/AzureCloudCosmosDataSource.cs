d3864b37d9a25334225909738bb72d5752f5a06b
fahad@thetechidea.com <fahad@thetechidea.com>
2022-05-02 20:13:04 +0300

    update DataSource and References

--- DataSourcesPlugins/AzureCloudDataSource/AzureCloudCosmosDataSource.cs
+++ DataSourcesPlugins/AzureCloudDataSource/AzureCloudCosmosDataSource.cs
@@ -8,8 +8,6 @@ using TheTechIdea.Beep.Vis;
 using TheTechIdea.Beep.DataBase;
 using TheTechIdea.Beep.Editor;
 using TheTechIdea.Beep.Report;
-using TheTechIdea.Beep.Workflow;
-using TheTechIdea.Beep.Vis;
 using TheTechIdea.Logger;
 using TheTechIdea.Util;
 
