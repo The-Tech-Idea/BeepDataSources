5c125fd0ff7129d0cf1bd7b27d734bd56c780ef1
fahad@thetechidea.com <fahad@thetechidea.com>
2023-04-23 21:35:20 +0300

    update python

---
+++ OPCUADataSource/OPCDataSource.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using TheTechIdea;
+using TheTechIdea.Beep;
+using TheTechIdea.Beep.DataBase;
+using TheTechIdea.Beep.Editor;
+using TheTechIdea.Beep.Report;
+using TheTechIdea.Logger;
+using TheTechIdea.Util;
+
+namespace OPCUADataSource
+{
+    public class OPCDataSource : IDataSource
+    {
+        public DataSourceType DatasourceType { get  ; set  ; }
+        public DatasourceCategory Category { get  ; set  ; }
+        public IDataConnection Dataconnection { get  ; set  ; }
+        public string DatasourceName { get  ; set  ; }
+        public IErrorsInfo ErrorObject { get  ; set  ; }
+        public string Id { get  ; set  ; }
+        public IDMLogger Logger { get  ; set  ; }
+        public List<string> EntitiesNames { get  ; set  ; }
+        public List<EntityStructure> Entities { get  ; set  ; }
+        public IDMEEditor DMEEditor { get  ; set  ; }
+        public ConnectionState ConnectionStatus { get  ; set  ; }
+        public string ColumnDelimiter { get  ; set  ; }
+        public string ParameterDelimiter { get  ; set  ; }
+
+        public event EventHandler<PassedArgs> PassEvent;
+
+        public IErrorsInfo BeginTransaction(PassedArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckEntityExist(string EntityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionState Closeconnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo Commit(PassedArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo CreateEntities(List<EntityStructure> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateEntityAs(EntityStructure entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo DeleteEntity(string EntityName, object UploadDataRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo EndTransaction(PassedArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo ExecuteSql(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ChildRelation> GetChildTablesList(string tablename, string SchemaName, string Filterparamters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ETLScriptDet> GetCreateEntityScript(List<EntityStructure> entities = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetEntitesList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetEntity(string EntityName, List<AppFilter> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetEntityAsync(string EntityName, List<AppFilter> Filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RelationShipKeys> GetEntityforeignkeys(string entityname, string SchemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetEntityIdx(string entityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EntityStructure GetEntityStructure(string EntityName, bool refresh)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EntityStructure GetEntityStructure(EntityStructure fnd, bool refresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetEntityType(string EntityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo InsertEntity(string EntityName, object InsertedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConnectionState Openconnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object RunQuery(string qrystr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo RunScript(ETLScriptDet dDLScripts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo UpdateEntities(string EntityName, object UploadData, IProgress<PassedArgs> progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IErrorsInfo UpdateEntity(string EntityName, object UploadDataRow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
